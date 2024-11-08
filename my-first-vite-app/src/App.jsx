58e77f92c9d0d16c4d892131c9691f72588b6b3e
wonheeki <wonluvv229@gmail.com>
2024-11-08 17:59:33 +0900

    counter 만들기 실습 완료

--- my-first-vite-app/src/App.jsx
+++ my-first-vite-app/src/App.jsx
@@ -1,29 +1,63 @@
-import React from 'react'
+// import React from "react";
+
+// function Child(props) {
+//   return <div>{props.grandFatherName}</div>;
+// }
+
+// function Mother(props) {
+//   return <Child grandFatherName={props.grandFatherName} />;
+// }
+
+// function GrandFather() {
+//   const name = "르탄이";
+//   return <Mother grandFatherName={name} />;
+// }
+
+// function App() {
+//   return <GrandFather />;
+// }
+
+// export default App;
+
+import React, { useState } from 'react'
 
 const App = () => {
-  const NUMBER =1000;
-  const arr =[1,2,3,4,5];
+  // const [name, setName]= useState("르순이");
+  // const [text, setText]= useState("");
 
-  const pTagStyle={
-    color:"orange",
-    fontSize:"20px", 
-  };
+  // const handleNameChange =()=>{
+  //   setName("최르순이");
+  // }
 
+  // const handleInputChange=(event)=>{
+  //   console.log(event)
+  //   setText(event.target.value);
+  // }
+  const [number, setNumber]= useState(0);
+  const plusBtn =()=>{
+    setNumber(number+1);
+  }
+  const minusBtn=()=>{
+    setNumber(number-1);
+  }
   return (
-    <>
-      <div
-        id="abc"
-        className="abc"
-      >첫 번째 줄</div>
-      <div>두 번째 줄</div>
-      <div>{NUMBER}</div>
+    // <div>
+    //   <h1>React State</h1>
+    //   <button onClick={handleNameChange}>최르순이로!</button>
+    //   {name}
+    //   <br/>
 
-      <p style={{color:"orange"}}>하이</p>
-
-      <p style={pTagStyle}>하이하이</p>
-    </>
+    //   <input onChange={handleInputChange}/>
+    //   <br/>
+    //   {text}
+    // </div>
+   <>
+   <div>{number}</div>
+   <br/>
+    <button onClick={plusBtn}>+1</button>
+    <button onClick={minusBtn}>-1</button>
+   </> 
   )
 }
 
 export default App
-  
